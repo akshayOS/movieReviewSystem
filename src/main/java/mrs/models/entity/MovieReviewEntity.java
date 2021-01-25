f97ab232bb08b856a8628f81c4e2da0f80b9867f
akshayOS <33590662+akshayOS@users.noreply.github.com>
2021-01-26 03:29:17 +0530

    Update MovieReviewEntity.java

--- src/main/java/mrs/models/entity/MovieReviewEntity.java
+++ src/main/java/mrs/models/entity/MovieReviewEntity.java
@@ -1,5 +1,6 @@
 package mrs.models.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,21 +12,30 @@ import java.io.Serializable;
 @Table(name = "movie_review_details")
 @Data
 @Builder
-public class MovieReviewEntity implements Serializable {
+@NoArgsConstructor
+@AllArgsConstructor
+public class MovieReviewEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Integer id;
 
+    @Column(name = "review_score")
     private Integer reviewScore;
 
+    @Column(name = "review_capability")
     private String reviewerCapability;
 
+    @Column(name = "movie_name")
     private String movieName;
 
+    @Column(name = "release_year")
     private Integer releaseYear;
 
+    @Column(name = "movie_genre")
     private String movieGenre;
 
+    @Column(name = "user_name")
     private String userName;
 }
