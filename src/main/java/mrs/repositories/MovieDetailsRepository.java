d4b8e2687f1857e21505d1ef9ef627121aa66518
akshayOS <33590662+akshayOS@users.noreply.github.com>
2021-01-26 03:30:20 +0530

    Update MovieDetailsRepository.java

--- src/main/java/mrs/repositories/MovieDetailsRepository.java
+++ src/main/java/mrs/repositories/MovieDetailsRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 @Repository
 public interface MovieDetailsRepository extends JpaRepository<MovieEntity, Integer> {
 
-    Optional<MovieEntity> findByMovieName(final String name);
+    Optional<MovieEntity> findFirstByMovieName(final String name);
 }
