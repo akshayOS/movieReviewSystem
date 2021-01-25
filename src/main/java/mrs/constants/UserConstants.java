bbe005a28a2347195ed72d234e9f3f5572cef82d
akshayOS <33590662+akshayOS@users.noreply.github.com>
2021-01-26 01:35:02 +0530

    Create UserConstants.java

---
+++ src/main/java/mrs/constants/UserConstants.java
@@ -0,0 +1,11 @@
+package mrs.constants;
+
+public class UserConstants {
+
+    public static final String USER_CAPABILITY_VIEWER = "viewer";
+
+    public static final String USER_CAPABILITY_CRITIC = "critic";
+
+    public static final Integer USER_THRESHOLD_COUNT_FOR_CRITIC = 3;
+
+}
